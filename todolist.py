cab44ff672dc5cbde5e380ab3eabe720bc5677e8
moccil-bzz <moccil@bzz.com>
2024-01-15 08:13:41 +0100

    efficient correction

--- todolist.py
+++ todolist.py
@@ -18,7 +18,7 @@ def questions():
               'EXIT PROGRAM: 4\n')
         userinput = input()
         if userinput == '1':
-            load()
+            show()
             break
         elif userinput == '2':
             add()
@@ -39,20 +39,23 @@ def load():
             print('LOADING DATA...')
             with open('todos.json', 'r') as file:
                 try:
-                    data = json.load(file)
+                    return json.load(file)
                 except json.JSONDecodeError:
                     print('ERROR WHILE READING DATA')
                     print('EXITING FUNCTION')
-                    break
-                print('\n\nTO-DO LIST')
-                print('-------------------------------')
-                for i in data:
-                    print('- ' + i["content"])
-                input('\n\n[ENTER] TO STOP WATCHING')
-                break
+                    return []
         else:
             print('FILE NOT FOUND. CREATING A NEW FILE...')
             create()
+
+
+def show():
+    data = load()
+    print('\n\nTO-DO LIST')
+    print('-------------------------------')
+    for i in data:
+        print('- ' + i["content"])
+    input('\n\n[ENTER] TO STOP WATCHING')
     questions()
 
 
@@ -68,63 +71,43 @@ def add():
         if task == ' ' * len(task):
             print('YOU MUST ATLEAST WRITE ONE CHARACTER')
         else:
-            if os.path.exists('todos.json'):
-                with open('todos.json', 'r') as file:
-                    try:
-                        tasks = json.load(file)
-                    except json.JSONDecodeError:
-                        print('ERROR WHILE READING DATA')
-                        print('EXITING FUNCTION')
-                        break
-            else:
-                tasks = []
+            tasks = load()
             item = {"id": str(time.time()), "content": str(task)}
             tasks.append(item)
             with open('todos.json', 'w') as file:
                 json.dump(tasks, file)
             print('NEW TASK ADDED')
-            load()
+            show()
             break
 
 
 def delete():
+    data = load()
     while True:
-        if os.path.exists('todos.json'):
-            print('LOADING DATA...')
-            with open('todos.json', 'r') as file:
-                try:
-                    data = json.load(file)
-                except json.JSONDecodeError:
-                    print('ERROR WHILE READING DATA')
-                    print('EXITING FUNCTION')
-                    break
-            if len(data) == 0:
-                print('NO TASKS FOUND')
-                break
-            print('\n\nTO-DO LIST')
-            print('-------------------------------')
-            for index, item in enumerate(data, start=1):
-                print(f'{index}. {item["content"]}')
-            userinput = input('\nCHOOSE WHICH TASK YOU WANT TO DELETE / [ENTER] TO CANCEL: ')
-            #print(data)
-            found = False
-            for index, item in enumerate(data, start=1):
-                if str(index) == userinput:
-                    found = True
-                    #print(f'{userinput} {index}')
-                    #print(item["id"])
-                    del data[index -1]
-                    print('TASK DELETED')
-            if not found:
-                print('NOTHING FOUND TO DELETE RETURNING TO TO-DO LIST')
-                break
-            with open('todos.json', 'w') as file:
-                json.dump(data, file)
+        if len(data) == 0:
+            print('NO TASKS FOUND')
             break
-        else:
-            print('FILE NOT FOUND. CREATING A NEW FILE...')
-            create()
-    load()
+        print('\n\nTO-DO LIST')
+        print('-------------------------------')
+        for index, item in enumerate(data, start=1):
+            print(f'{index}. {item["content"]}')
+        userinput = input('\nCHOOSE WHICH TASK YOU WANT TO DELETE / [ENTER] TO CANCEL: ')
+        #print(data)
+        found = False
+        for index, item in enumerate(data, start=1):
+            if str(index) == userinput:
+                found = True
+                #print(f'{userinput} {index}')
+                #print(item["id"])
+                del data[index -1]
+                print('TASK DELETED')
+        if not found:
+            print('NOTHING FOUND TO DELETE RETURNING TO TO-DO LIST')
+            break
+        with open('todos.json', 'w') as file:
+            json.dump(data, file)
+        break
+    show()
 
 
 if __name__ == '__main__':
